fc4bc27736f6b1650220a57d776b47dfe2c77b98
mkafwanka <kaf23001@byui.edu>
2024-03-03 18:21:33 -0800

    finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int score = int.Parse(answer);
+
+        string letter = GetLetter(score);
+        
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations, well done!!!");
+        }
+        else
+        {
+            Console.WriteLine("To pass this class, you need C- but never give up, try again!!");
+        }
+    }
+
+    static string GetLetter(int score)
+    {
+        string letter = "";
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (score % 10 >= 7)
+        {
+            letter += "+";
+        }
+        else if (score % 10 < 3 && score != 100)
+        {
+            letter += "-";
+        }
+
+        return letter;
     }
 }
+   
